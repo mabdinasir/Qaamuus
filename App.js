ef6f8b474741096a848681f1efb745eea337d661
Abdelnacerr <abdelnacerr@gmail.com>
2021-02-23 15:23:37 +1100

    onBoarding Screen

--- App.js
+++ App.js
@@ -1,4 +1,3 @@
-import { StatusBar } from "expo-status-bar";
 import React from "react";
 
 import Tabs from "./Routes/Tabs";
---
+++ screens/Onboarding.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+
+export default function Onboarding() {
+  return (
+    <View>
+      <Text>Hello</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({});
