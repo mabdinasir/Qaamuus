fa97b81a5b2b9bdc7d9a126a3e4146b20b861743
Abdelnacerr <abdelnacerr@gmail.com>
2021-02-24 14:49:38 +1100

    activityLoader

--- App.js
+++ App.js
@@ -1,8 +1,41 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { ActivityIndicator, View, StyleSheet } from "react-native";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
 import Tabs from "./Routes/Tabs";
 import Onboarding from "./screens/Onboarding";
 
-export default function App() {
-  return <Onboarding />;
+const Loading = () => {
+  return (
+    <View>
+      <ActivityIndicator size="large" color= 'brown'/>
+    </View>
+  );
+};
+
+const checkOnboarding = async () => {
+  try {
+    
+  } catch {
+    
+  } finally {
+
+  }
 }
+
+export default function App() {
+  const [loading, setLoading] = useState(true);
+  const [viewedOnBoarding, setviewedOnBoarding] = useState(false);
+  return (
+    <View style={styles.container}>
+      {loading ? <Loading /> : viewedOnBoarding ? <Tabs /> : <Onboarding />}
+    </View>
+  );
+}
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+});
